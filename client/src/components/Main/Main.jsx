36d5faa4d99b6768919218e399a14af58237ed1e
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-10-05 02:43:50 +0800

    refactor codes

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -43,105 +43,6 @@ export default function Main({currentSession, isSending, setIsSending}) {
         }
     },[currentSession])
 
-    // handle  current note
-    function handleCurrentNote(e) {
-        const { name, value } = e.target;
-        setCurrentNote(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        })
-    }
-
-    // new note
-    const [newNote, setNewNote] = useState({
-        title: '',
-        body: '',
-    })
-    // handle new note
-    function handleNewNote(e) {
-        const { name, value} = e.target;
-        setNewNote(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        })
-    }
-
-    // state of is Edit or is Add
-    const [isAdd, setIsAdd] = useState(false)
-    const [isEdit, setIsEdit] = useState(false)
-    
-    function toggleIsAdd() {
-        setIsSending(prev => !prev)
-        setIsAdd(prev => !prev)
-    };
-
-    function toggleIsEdit() {
-        setIsSending(prev => !prev)
-        setIsEdit(prev => !prev)
-    };
-
-    // add new note 
-    async function addNote(event) {
-        event.preventDefault();
-        togglePlayerOff()
-        const time = Math.floor(playerRef.current.getCurrentTime());
-        const sessionId = currentSession._id
-        setIsSending(true)
-        await axios.post("http://localhost:3001/add/note", {...newNote, time, sessionId}).catch(error => console.log(error))
-                    .then(res => {
-                        setCurrentNote(res.data);
-                        setNotes([res.data, ...notes]);
-                    })
-        setNewNote({title: '', body: ''})
-        toggleIsAdd();
-    }
-
-    // delete new note
-    async function deleteNote(event) {
-        event.preventDefault();
-        const sessionId = currentSession._id
-        const noteId = currentNote._id
-        setIsSending(true)
-        await axios.post("http://localhost:3001/delete/note", {sessionId, noteId}).catch(error => console.log(error))
-                    .then(res => {
-                        console.log(res.data)
-                        setNotes(notes => {
-                            const filter = notes.filter(note => note._id !== noteId)
-                            if (!(filter.includes(currentNote)) && filter.length !== 0) setCurrentNote(filter[0]);
-                            if (filter.length === 0 ) setCurrentNote(baseNote);
-                            return filter;
-                    });
-        })
-        setIsSending(false)
-    }
-
-    // edit note 
-    async function editNote(event) {
-        event.preventDefault()
-        togglePlayerOff()
-        const time = Math.floor(playerRef.current.getCurrentTime());
-        const sessionId = currentSession._id;
-        setIsSending(true);
-        await axios.post("http://localhost:3001/update/note", {...currentNote, time, sessionId}).catch(error => console.log(error))
-        .then(res => {
-            console.log(res.data)
-            setNotes(notes => {
-                const findIndex = notes.findIndex(el => el._id === currentNote._id)
-                notes[findIndex].title = currentNote.title;
-                notes[findIndex].body = currentNote.body;
-                notes[findIndex].time = time;
-                setCurrentNote(notes[findIndex])
-                return notes;
-            })
-
-        })
-        toggleIsEdit();
-    }
-  
     // return main
     if (currentNote === undefined) {
         return <>Still Loading....</>
@@ -156,17 +57,15 @@ export default function Main({currentSession, isSending, setIsSending}) {
             />
             <Note 
                 currentNote={currentNote}
-                handleCurrentNote={handleCurrentNote}
+                setCurrentNote={setCurrentNote}
+                notes={notes}
+                setNotes={setNotes}
+                isSending={isSending}
                 setIsSending={setIsSending}
-                newNote={newNote}
-                handleNewNote={handleNewNote}
-                isAdd={isAdd}
-                toggleIsAdd={toggleIsAdd}
-                isEdit={isEdit}
-                toggleIsEdit={toggleIsEdit}
-                addNote={addNote}
-                deleteNote={deleteNote}
-                editNote={editNote}
+                currentSession={currentSession}
+                playerRef={playerRef}
+                togglePlayerOff={togglePlayerOff}
+                baseNote={baseNote}
             />
             <NoteList 
                 notes={notes}
